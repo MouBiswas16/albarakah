bcc3eb44525a37ea59f152605d7ca92f8b504c98
Mou Biswas <qtmeaowmou16@gmail.com>
2025-03-17 01:08:05 +0600

    modified login screen

--- lib/core/export/export.dart
+++ lib/core/export/export.dart
@@ -1,3 +1,4 @@
 export 'package:flutter/material.dart';
 export 'package:flutter_native_splash/flutter_native_splash.dart';
 export 'package:albarakah/screens/login.dart';
+export 'package:albarakah/core/utils/constans.dart';
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,144 +1,185 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
 
 import 'package:albarakah/core/export/export.dart';
-import 'package:albarakah/core/utils/constans.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isSelected = true;
+  bool isPasswordVisiable = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(18),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.09,
-              width: MediaQuery.of(context).size.height * 0.9,
-              child: Image.asset(
-                "assets/images/albarakah.png",
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.09),
-            Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Text(
-                "Email",
-                style: TextStyle(
-                  color: textColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.width * 0.09),
+              /*            App Logo Here            */
+              Center(
+                child: SizedBox(
+                  height: 270,
+                  width: MediaQuery.of(context).size.width * 0.54,
+                  child: appLogo,
                 ),
               ),
-            ),
-            SizedBox(height: 8),
-            TextField(
-              decoration: InputDecoration(
-                fillColor: backgroundColor,
-                filled: true,
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.email),
-                ),
-                suffixIconColor: textColor,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                labelText: "Email",
-                labelStyle: TextStyle(
-                  color: textColor,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: primaryColor),
-                ),
-              ),
-            ),
-            SizedBox(height: 18),
-            Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Text(
-                "Password",
-                style: TextStyle(
-                  color: textColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            SizedBox(height: 8),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                fillColor: backgroundColor,
-                filled: true,
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.remove_red_eye),
-                ),
-                suffixIconColor: textColor,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                labelText: "Enter Your Password Here",
-                labelStyle: TextStyle(
-                  color: textColor,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: primaryColor),
-                ),
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.025),
-            Center(
-              child: Column(
-                children: [
-                  ElevatedButton(
+              SizedBox(height: MediaQuery.of(context).size.width * 0.09),
+              SizedBox(height: 8),
+              /*            Phone Number Here            */
+              TextField(
+                onTap: () {
+                  isSelected = !isSelected;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  prefixIcon: IconButton(
                     onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        side: BorderSide(
-                          width: 3,
-                          color: primaryColor,
-                        )),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 12, bottom: 12, left: 18, right: 18),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(color: primaryColor, fontSize: 24),
+                    icon: Icon(Icons.smartphone),
+                  ),
+                  prefixIconColor: !isSelected ? Colors.blue : textColor,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: backgroundColor),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: backgroundColor),
+                  ),
+                  labelText: "01xxxxxxxxx",
+                  labelStyle: TextStyle(
+                    color: textColor,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: backgroundColor),
+                  ),
+                ),
+              ),
+              SizedBox(height: 18),
+              /*            Password Here            */
+              TextField(
+                obscureText: !isPasswordVisiable,
+                onTap: () {
+                  isPasswordVisiable = !isPasswordVisiable;
+                },
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  prefixIcon: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.key),
+                  ),
+                  prefixIconColor: isPasswordVisiable ? Colors.blue : textColor,
+                  suffixIcon: isPasswordVisiable
+                      ? Icon(Icons.remove_red_eye)
+                      : Icon(Icons.visibility_off),
+                  suffixIconColor: isPasswordVisiable ? Colors.blue : textColor,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: backgroundColor),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: backgroundColor),
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: textColor,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: backgroundColor),
+                  ),
+                ),
+              ),
+              SizedBox(height: 12),
+              Center(
+                child: Column(
+                  children: [
+                    /*            Login button Here            */
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(28),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: Container(
+                              // width: MediaQuery.of(context).size.width * 100,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(colors: [
+                                  Colors.cyan,
+                                  Colors.blue,
+                                ]),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              padding: EdgeInsets.only(
+                                top: 8,
+                                bottom: 8,
+                                left: MediaQuery.of(context).size.width / 3,
+                                right: MediaQuery.of(context).size.width / 3,
+                              ),
+                              foregroundColor: Colors.white,
+                              textStyle: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            child: Text("LOGIN"),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 18),
-                  RichText(
-                    text: TextSpan(
-                      style: TextStyle(color: textColor, fontSize: 14),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: "Not Have an Account Yet? ",
-                          style: TextStyle(color: primaryColor),
+                    SizedBox(height: 8),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Forget Password",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber,
                         ),
-                        TextSpan(
-                            text: "Create one Now!!!",
-                            style: TextStyle(fontWeight: FontWeight.bold))
-                      ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    "Go to the Sign Up screen.",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: textColor,
+                    // SizedBox(height: 2),
+                    /*            Acccount login or Signup            */
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: textColor, fontSize: 14),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "Don't have an account? ",
+                          ),
+                          TextSpan(
+                            text: "Register",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: primaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 12),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
